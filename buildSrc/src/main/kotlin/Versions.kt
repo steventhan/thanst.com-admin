5545450dfdf500f8dfb72446891c959f78ce6763
Steven Than <steventhan11@gmail.com>
2019-12-23 21:59:15 -0500

    more versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,5 +3,8 @@ object Versions {
     const val spring = "5.2.2.RELEASE"
     const val springBoot = "2.2.2.RELEASE"
     const val springDependencyMgmt = "1.0.8.RELEASE"
+    const val gradleNodePlugin = "1.3.1"
+    const val node = "12.14.0"
+    const val yarn = "1.21.1"
 }
 
