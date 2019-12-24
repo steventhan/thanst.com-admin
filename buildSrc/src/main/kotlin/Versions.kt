ed3c03bb3a2f7a126889cdb7f8b252a83efc4759
Steven Than <steventhan11@gmail.com>
2019-12-23 21:38:12 -0500

    added versions vars

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,4 +1,7 @@
-object DependencyVersions {
-    const val kotlinVersion = "1.3.61"
+object Versions {
+    const val kotlin = "1.3.61"
+    const val spring = "5.2.2.RELEASE"
+    const val springBoot = "2.2.2.RELEASE"
+    const val springDependencyMgmt = "1.0.8.RELEASE"
 }
 
