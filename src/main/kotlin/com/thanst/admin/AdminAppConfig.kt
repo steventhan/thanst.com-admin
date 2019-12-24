cdb15a890cb0b10207ec964fdc1b0ec91d8564e5
Steven Than <steventhan11@gmail.com>
2019-12-24 06:09:52 -0500

    removed unused config, added resource mappings

--- src/main/kotlin/com/thanst/admin/AdminAppConfig.kt
+++ src/main/kotlin/com/thanst/admin/AdminAppConfig.kt
@@ -1,16 +1,19 @@
 package com.thanst.admin
 
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.ResourceHandlerRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
-@EnableWebFlux
-@ComponentScan(basePackages = ["com.thanst.admin"])
 class AdminAppConfig : WebFluxConfigurer {
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations( "classpath:/static/js/")
+    }
+
     @Bean
     fun webClient(): WebClient = WebClient.create()
 
