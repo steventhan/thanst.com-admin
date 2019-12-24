3c62e92f20259f6b193046a54ab1647d5df3d910
Steven Than <steventhan11@gmail.com>
2019-12-23 21:37:53 -0500

    updated build.gradle.kts, added spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version DependencyVersions.kotlinVersion
+    kotlin("jvm") version Versions.kotlin
+    id("org.jetbrains.kotlin.plugin.spring") version Versions.kotlin
+    id("org.springframework.boot") version Versions.springBoot
+    id("io.spring.dependency-management") version Versions.springDependencyMgmt
+
 }
 
+group = "com.thanst.admin"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+val developmentOnly by configurations.creating
+configurations {
+    runtimeClasspath {
+        extendsFrom(developmentOnly)
+    }
+}
 
 repositories {
     mavenLocal()
     mavenCentral()
 }
 
-
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:${DependencyVersions.kotlinVersion}")
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    // Development dependencies
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // Test dependencies
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation("io.projectreactor:reactor-test")
+
+
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+    }
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
 }
