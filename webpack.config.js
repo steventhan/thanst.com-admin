c209dd3b74f5a50222137683c738a6f97da660e1
Steven Than <steventhan11@gmail.com>
2019-12-24 06:07:11 -0500

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const REACT_PATH = path.resolve(__dirname, "src/main/react");
+const OUTPUT_PATH = path.resolve(__dirname, "build/resources/static");
 
 const config = {
     entry: path.join(REACT_PATH, "index.tsx"),
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
@@ -18,8 +21,14 @@ const config = {
     },
     output: {
         filename: "js/bundle.js",
-        path: path.resolve(__dirname, "build/resources/assets"),
+        path: OUTPUT_PATH,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(REACT_PATH, "public/index.html"),
+            filename: path.join(OUTPUT_PATH, "index.html")
+        })
+    ]
 };
 
 module.exports = config;
