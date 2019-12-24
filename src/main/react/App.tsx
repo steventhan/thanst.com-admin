7bdcd6193ef05c3a00493fa6ffd7bdff02aa4afb
Steven Than <steventhan11@gmail.com>
2019-12-24 06:10:17 -0500

    react stuff

---
+++ src/main/react/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const App: FC = () => {
+    return (
+        <div className="App">
+            hello world
+        </div>
+    );
+};
+
+export default App;
