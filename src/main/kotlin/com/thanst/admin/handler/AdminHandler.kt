6a4f72a7bf9d677a36420cd0563f6f07e8ff8ea0
Steven Than <steventhan11@gmail.com>
2019-12-29 03:34:04 -0500

    simplified handler

--- src/main/kotlin/com/thanst/admin/handler/AdminHandler.kt
+++ src/main/kotlin/com/thanst/admin/handler/AdminHandler.kt
@@ -1,32 +1,13 @@
 package com.thanst.admin.handler
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.core.context.ReactiveSecurityContextHolder
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
-import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User
-import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.buildAndAwait
 import reactor.core.publisher.Mono
-import java.security.Principal
 
 @Component
 class AdminHandler {
     fun adminPage(req: ServerRequest): Mono<ServerResponse> {
-        println("in handler")
-        return req.principal()
-                .flatMap { maybeOAuthToken ->
-                    when(maybeOAuthToken) {
-                        is OAuth2AuthenticationToken -> {
-                            ServerResponse.ok().render("index")
-                        }
-                        else -> ServerResponse.badRequest().build()
-                    }
-                }
+        return ServerResponse.ok().render("index")
     }
 }
