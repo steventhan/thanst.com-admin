eac494ed242d297ca125ae2632b9cb125e302f00
Steven Than <steventhan11@gmail.com>
2019-12-29 03:05:02 -0500

    package for filters

---
+++ src/main/kotlin/com/thanst/admin/filter/AdminFilter.kt
@@ -0,0 +1,14 @@
+package com.thanst.admin.filter
+
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+
+@Component
+class AdminFilter : WebFilter {
+    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+        return chain.filter(exchange)
+    }
+}
