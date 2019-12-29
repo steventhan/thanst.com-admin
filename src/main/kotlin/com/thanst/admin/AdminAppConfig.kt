b19acfdfc7fe212c96aa984d713bb351f58af770
Steven Than <steventhan11@gmail.com>
2019-12-29 03:11:28 -0500

    provide bean to the main react view, the bean's responsible for injecting initial state

--- src/main/kotlin/com/thanst/admin/AdminAppConfig.kt
+++ src/main/kotlin/com/thanst/admin/AdminAppConfig.kt
@@ -1,10 +1,18 @@
 package com.thanst.admin
 
+import com.thanst.admin.model.InitialState
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Scope
+import org.springframework.http.MediaType
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.web.reactive.config.ResourceHandlerRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.server.ServerWebExchange
+import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveView
+import reactor.core.publisher.Mono
+import kotlin.collections.LinkedHashMap
 
 @Configuration
 class AdminAppConfig : WebFluxConfigurer {
@@ -17,4 +25,16 @@ class AdminAppConfig : WebFluxConfigurer {
     @Bean
     fun webClient(): WebClient = WebClient.create()
 
+    @Bean
+    @Scope("prototype")
+    fun index(): ThymeleafReactiveView = object : ThymeleafReactiveView() {
+        override fun render(model: MutableMap<String, *>?, contentType: MediaType?,
+                            exchange: ServerWebExchange): Mono<Void> {
+            val newModel = LinkedHashMap(model)
+            return exchange.getPrincipal<OAuth2AuthenticationToken>().flatMap { it ->
+                newModel["initialState"] = InitialState(oauth2User = it.principal).toJson()
+                super.render(newModel, contentType, exchange)
+            }
+        }
+    }
 }
