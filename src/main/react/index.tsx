a77dcfe563ec0d58ba1be7cb35fde3f7fcc25877
Steven Than <steventhan11@gmail.com>
2019-12-29 03:20:43 -0500

    read initial state and add to a context

--- src/main/react/index.tsx
+++ src/main/react/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+interface Window {
+    __INITIAL_STATE__: string
+}
+
+interface InitialState {
+    [key: string]: any
+}
+
+declare const window: Window;
+const initialState: InitialState = JSON.parse(window.__INITIAL_STATE__);
+const InitialStateContext = createContext(initialState);
+
+
+ReactDOM.render(
+    <InitialStateContext.Provider value={initialState}>
+        <App />
+    </InitialStateContext.Provider>,
+    document.getElementById('root')
+);
 
 serviceWorker.unregister();
+
+export {
+    InitialStateContext
+}
